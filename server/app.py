f360706313bd7925126aa6ff16afe3d54369f0b8
Anchit <anc.shukla@gmail.com>
2018-03-13 11:13:14 +0530

    Run index template from flask

---
+++ server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+serve_dir = os.path.abspath('../public')
+app = Flask(__name__, template_folder=serve_dir, static_folder=serve_dir)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+# @app.route('/js/<path:path>')
+# def send_js(path): return send_from_directory('js', path)
